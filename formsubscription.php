2c773010bb17706e6d82aef3281cb1eb8c9b3a41
hadrien-dhyne <hadrien.dhyne@apprenant.ifapme.be>
2024-09-28 16:39:32 +0200

    Creation of header.php

--- formsubscription.php
+++ formsubscription.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Réserve ta bringue</title>
-    <link rel="stylesheet" href="resources/css/reset.css" />
-    <link rel="stylesheet" href="resources/css/style.css" />
-    <link rel="stylesheet" href="https://use.typekit.net/vzq1zfl.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-
+<?php
+$title = "Réserve ta bringue";
+include "header.php";
+session_start();
+?>
   <body>
     <div class="form">
       <div>
